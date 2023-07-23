aeeda03c7d90c0b399312056062df003e7035f5a
redfe <tetsu999@gmail.com>
2023-07-23 16:51:55 +0900

    add GitHub Repo link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,9 @@
 		<a href={`${base}/`}>Home</a>
 		<a href={`${base}/simple`}>Simple</a>
 		<a href={`${base}/datetime`}>Datetime</a>
+		<a href="https://github.com/redfe/svelte-dynamic-scroll" target="_blank"
+			><img src="/github-mark.svg" title="GitHub Repo" alt="GitHub Repo" /></a
+		>
 	</nav>
 	<main>
 		<slot />
@@ -33,8 +36,19 @@
 	}
 	nav {
 		height: 30px;
+		display: flex;
+		column-gap: 10px;
+		margin-top: 10px;
+	}
+	nav a:last-child {
+		flex-grow: 1;
+		text-align: right;
 	}
 	main {
-		height: calc(100vh - 40px);
+		height: calc(100vh - 50px);
+	}
+	img {
+		width: 20px;
+		height: 20px;
 	}
 </style>
