3cfa4b4ca9e4472a79c5c706eed5f0226a985f3b
redfe <tetsu999@gmail.com>
2023-07-27 22:04:39 +0900

    modify

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,10 @@
 		<a href={`${base}/`}>Home</a>
 		<a href={`${base}/simple`}>Simple</a>
 		<a href={`${base}/datetime`}>Datetime</a>
-		<a href={`${base}/storybook-static`}>Storybook</a>
+		<span />
+		<a href={`${base}/storybook-static`} target="_blank"
+			><img src={`${base}/storybook.svg`} title="Storybook Tests" alt="Storybook Tests" /></a
+		>
 		<a href="https://github.com/redfe/svelte-dynamic-scroll" target="_blank"
 			><img src={`${base}/github-mark.svg`} title="GitHub Repo" alt="GitHub Repo" /></a
 		>
@@ -33,20 +36,26 @@
 		justify-content: center;
 	}
 	.top {
-		width: 500px;
+		width: 100%;
+		max-width: 500px;
 	}
 	nav {
 		height: 30px;
 		display: flex;
 		column-gap: 10px;
 		margin-top: 10px;
+		padding: 0 20px;
+		box-sizing: border-box;
 	}
-	nav a:nth-child(4) {
+	nav *:nth-child(4) {
 		flex-grow: 1;
 		text-align: right;
 	}
 	main {
-		height: calc(100vh - 50px);
+		width: 100%;
+		height: calc(100vh - 70px);
+		padding: 20px;
+		box-sizing: border-box;
 	}
 	img {
 		width: 20px;
